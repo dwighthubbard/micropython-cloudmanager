b56a04cec0314f2ff887d00188d9bf1e98937c58
Dwight Hubbard <d@d-h.us>
2016-07-02 01:18:51 -0700

    Version that works in concert with the micropython-redis-cloudclient

---
+++ setup.py
@@ -0,0 +1,24 @@
+import sys
+# Remove current dir from sys.path, otherwise setuptools will peek up our
+# module instead of system.
+sys.path.pop(0)
+from setuptools import setup
+
+
+setup(
+    name='micropython-cloudmanager',
+    description='Micropython client that allows network attached boards to be controlled from a central redis server',
+    long_description="""Provides a client that allows one a network capable micropython board to
+interface with a central redis server.""",
+    url='https://github.com/dhubbard/micropython-cloudmanager',
+    author='Dwight Hubbard',
+    author_email="dwight@dwighthubbard.com",
+    install_requires=['redislite'],
+    license='MIT',
+    maintainer='Dwight Hubbard',
+    maintainer_email='dwight@dwighthubbard.com',
+    packages=['cloudmanager'],
+    scripts=['scripts/micropython_board_manager'],
+    version='0.0.4',
+    zip_safe=True,
+)
