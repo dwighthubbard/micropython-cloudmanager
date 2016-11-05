d0ed9726316343396ba539b8f3b0d08579f67646
Dwight Hubbard <d@dhub.me>
2016-11-05 15:26:31 -0700

    Read the file as a bytestream

--- setup.py
+++ setup.py
@@ -19,6 +19,6 @@ interface with a central redis server.""",
     maintainer_email='dwight@dwighthubbard.com',
     packages=['cloudmanager'],
     scripts=['scripts/micropython_board_manager', 'scripts/mbm'],
-    version='0.0.48',
+    version='0.0.49',
     zip_safe=True,
 )
