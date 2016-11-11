9d56bfe79853ed1e0302658bab2a28a5abe7f25f
Dwight Hubbard <d@dhub.me>
2016-11-10 22:22:54 -0800

    Code cleanups
    Upload is now working
    
    Added macro function to allow creating commands on the server side tht do multiple operations on the nodes.

--- cloudmanager/package.py
+++ cloudmanager/package.py
@@ -1,11 +1,45 @@
+import json
 import os
 import subprocess
 import sys
+import requests
+import tarfile
 import tempfile
 from .board import MicropythonBoards
 
 
 def install_package(package_name):
+    package_info = package_name.split('=')
+    package_version = None
+    package_name = package_info[0]
+    if len(package_info) == 2:
+        package_version = package_info[-1]
+    url = 'https://pypi.python.org/pypi/{package}/json'.format(package=package_name)
+    response = requests.get(url)
+    package = response.json()
+    if not package_version:
+        package_version = package['info']['version']
+    url = package['releases'][package_version]['url']
+    with tempfile.TemporaryDirectory() as tempdir:
+        response = requests.get(url)
+        with open(os.path.basename(url), 'wb') as file_handle:
+            file_handle.write(response.content)
+        tar = tarfile.open(os.path.basename(url))
+        tar.extractall()
+        tar.close()
+
+        for root, dirs, files in os.walk('.'):
+            for name in files:
+                filename = os.path.join(root, name)
+                if filename.endswith('.py'):
+                    if filename.startswith('./'):
+                        filename = filename[2:]
+                        dest = os.path.join('lib', filename)
+                        print(filename)
+                        # MicropythonBoards().upload(filename=filename, dest=dest, range=range)
+
+
+def install_package_pip(package_name):
     with tempfile.TemporaryDirectory() as tempdir:
         os.chdir(tempdir)
         command = 'pip install --system --target="{tempdir}" {package}'.format(executable=sys.executable, tempdir=tempdir, package=package_name)
