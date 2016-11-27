312eb7374d8d481d47dde7c62c7c2dff63fd00fb
Dwight Hubbard <d@dhub.me>
2016-11-27 10:34:02 -0800

    more logging

--- setup.py
+++ setup.py
@@ -18,6 +18,6 @@ setup(
     maintainer_email='dwight@dwighthubbard.com',
     packages=['cloudmanager'],
     scripts=['scripts/micropython_board_manager', 'scripts/mbm', 'scripts/mbm_sync'],
-    version='0.0.130',
+    version='0.0.131',
     zip_safe=True,
 )
