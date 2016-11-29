c5ffbadea3380a3099f17b1734f92d9b7b33e3bc
Dwight Hubbard <d@dhub.me>
2016-11-29 13:50:33 -0800

    Updated version

--- setup.py
+++ setup.py
@@ -18,6 +18,6 @@ setup(
     maintainer_email='dwight@dwighthubbard.com',
     packages=['cloudmanager'],
     scripts=['scripts/micropython_board_manager', 'scripts/mbm', 'scripts/mbm_sync'],
-    version='0.0.141',
+    version='0.0.142',
     zip_safe=True,
 )
