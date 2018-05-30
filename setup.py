1b41eeaf7f5a34a622826bf0030a9f5c45d1aefc
Dwight Hubbard <dhubbard@yahoo-inc.com>
2018-05-29 22:46:15 -0700

    Require python 3.6

--- setup.py
+++ setup.py
@@ -12,7 +12,16 @@ setup(
     url='https://github.com/dhubbard/micropython-cloudmanager',
     author='Dwight Hubbard',
     author_email="dwight@dwighthubbard.com",
-    install_requires=['hostlists', 'redislite', 'python-daemon', 'pip>=8.1.2', 'netifaces', 'watchdog', 'requests'],
+    install_requires=[
+        'python_version>="3.6"',
+        'hostlists',
+        'redislite',
+        'python-daemon',
+        'pip>=8.1.2',
+        'netifaces',
+        'watchdog',
+        'requests'
+    ],
     license='MIT',
     maintainer='Dwight Hubbard',
     maintainer_email='dwight@dwighthubbard.com',
